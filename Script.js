378cd7e13e3ff1fb664344a8d4baf83623656f94
radahairova <radahairova@gmail.com>
2026-06-03 05:15:14 +0400

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+// "Версия для слабовидящих"
+const blindBtn = document.getElementById('blindVersionBtn');
+if (blindBtn) {
+    blindBtn.addEventListener('click', function() {
+        alert('Версия для слабовидящих будет доступна в следующей версии сайта.');
+    });
+}
